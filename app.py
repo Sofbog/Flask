12ebb3090b82d23e3fc6b244be50dd514a3f0dea
Sofbog <Devilmaycrytrav1@gmail.com>
2022-01-26 16:45:40 +0200

    update contact.html and add requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import csv
 
-
 app = Flask(__name__)
 
 
@@ -9,13 +8,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
-def Submit_form():
+def submit_form():
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
@@ -30,19 +30,17 @@ def Submit_form():
 
 def write_data_to_file(data):
     with open('database.txt', mode='a') as database:
-       email = data['email']
-       subject = data['subject']
-       message = data['message']
-       file = database.write(f'\n{email},{subject},{message}')
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
 
 
 def write_data_to_csv(data):
     with open('database.csv', 'a', newline='') as database2:
-       email = data['email']
-       subject = data ['subject']
-       message = data['message']
-       csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-       csv_writer.writerow([email, subject, message])
-
-if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=80, DEBUG=True)
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
